c8968452bf30e95b30d529347ea03f2f76fae25b
Daniel Perez <daniel.perez@banregio.com>
2023-09-09 13:20:02 -0600

    Solucion leccion 17 - Pirncipios SOLID

--- CleanSolid/src/clean-code/03-funciones.ts
+++ CleanSolid/src/clean-code/03-funciones.ts
@@ -1,34 +1,35 @@
 (() => {
 
-  // función para obtener información de una película por Id
-  function getAllMovies(movieId: string) {
+  function getMovieById(movieId: string) {
     console.log({ movieId });
   }
 
-  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-  function getAllMovieActors(id: string) {
-    console.log({ id });
+  function getMovieCastById(movieId: string) {
+    console.log({ movieId });
   }
 
-  // funcion para obtener el bio del actor por el id
-  function getUsuario(ActorId: string) {
-    console.log({ ActorId });
+  function getActorBioById(actorId: string) {
+    console.log({ actorId });
   }
 
-  // Crear una película
-  function movie(title: string, description: string, rating: number, cast: string[]) {
+  interface MovieInformation {
+    cast       : string[];
+    description: string;
+    rating     : number;
+    title      : string;
+  }
+  function setMovie({title, description, rating, cast}: MovieInformation) {
     console.log({ title, description, rating, cast });
   }
 
-  // Crea un nuevo actor
-  function createActorIfActorNotExists(fullName: string, birthdate: Date): boolean {
+  function setActor(fullName: string, birthdate: Date): boolean {
 
     // tarea asincrona para verificar nombre
     // ..
     // ..
     if (fullName === 'fernando') return false;
 
-    console.log('Crear actor');
+    console.log('Crear actor', birthdate);
     return true;
 
   }
